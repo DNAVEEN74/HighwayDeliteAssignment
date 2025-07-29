34d313836c53f873d42a1dd61b9d5ef05f39a7f4
DNAVEEN74 <naveendurgam74@gmail.com>
2025-07-29 17:25:52 +0530

    vercel link updated

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -14,7 +14,7 @@ export enum ResponseStatus{
   Error = 401
 }
 
-const allowedOrigins: string[] = ['https://highway-delite-ass-git-e75107-naveendurgam74-gmailcoms-projects.vercel.app','http://localhost:5173'];
+const allowedOrigins: string[] = ['https://highway-delite-assignment-inky.vercel.app','http://localhost:5173'];
 
 const corsOptions = {
   origin: (origin: string | undefined, callback: (err: Error | null, allow?: boolean) => void ) => {
